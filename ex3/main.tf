521365ba69ede097007b75f6b0937cafc7095e37
91996 <mukunca50@gmail.com>
2023-04-15 20:36:09 +0530

    main.tf

---
+++ ex3/main.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "instances" {
+  for_each               = var.instances
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = each.value["type"]
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
+
+output "ec2" {
+  value = [for k, v in aws_instance.instances : "${k} - ${v.public_ip}"]
+}
+
+
